cd582c1b4ef67784c28619141997bc5410128ebc
Brianna Doubt <bri@briannadoubt.com>
2022-07-10 14:25:40 -0700

    Initial Commit

---
+++ DistributedChat/DistributedChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistributedChatApp.swift
+//  DistributedChat
+//
+//  Created by Bri on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DistributedChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
